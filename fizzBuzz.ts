4615ae05b1bb7768cdff8829286f33e5ef46a012
Mia Quinn <mia.quinn@UK-MQUINN.local>
2024-05-08 18:02:46 +0100

    bang fezz bong

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,15 +1,31 @@
 fizzbuzz();
 
 function fizzbuzz(){
-    for(let i = 0; i < 100; i++){
-        if(i % 3 == 0 && i % 5 == 0){
-            console.log("FizzBuzz");
-        }else if(i % 3 == 0){
-            console.log("Fizz");
-        }else if(i % 5 == 0){
-            console.log("Buzz");
-        }else{
+    let word = "";
+
+    for(let i = 1; i < 100; i++){
+        word = "";
+        if(i % 11 == 0){
+            console.log("Fezz");
+            continue;
+        }if(i % 3 == 0){
+            word = "Fizz";
+        }if(i % 5 == 0){
+            word = word + "Buzz";
+        }if(i % 7 == 0){
+            word = word + "Bang";
+        }if(i % 13 == 0){
+            if(!word.includes("B")){
+                word = word + "Bong"
+            }else{
+                let pos = word.indexOf("B");
+                word = word.slice(0, pos) + "Bong" + word.slice(pos);
+            }
+        }if(word.length < 1){
             console.log(i);
+            continue;
         }
+
+        console.log(word);
     }
 }
