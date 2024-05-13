d9f0d8ae631ecbb47c48053feb464b48746ec143
Mia Quinn <mia.quinn@tipalti.com>
2024-05-13 10:26:40 +0100

    bong

---
+++ fizzBuzz.js
@@ -0,0 +1,40 @@
+fizzbuzz();
+
+function fizzbuzz(){
+    let word = [];
+    let bIndex = 0;
+    for(let i = 1; i < 100; i++){
+        bIndex = 0;
+        word = [];
+        if(i % 11 == 0){
+            console.log("Fezz");
+            continue;
+        }if(i % 3 == 0){
+            word.push('Fizz');
+        }if(i % 5 == 0){
+            word.push('Buzz');
+            bIndex++;
+        }if(i % 7 == 0){
+            word.push('Bang');
+            bIndex++;
+        }if(i % 13 == 0){
+            if(bIndex == 0){
+                word.push('Bong')
+            }else{
+                const newWord = [
+                    ...word.slice(0, bIndex - 1),
+                    'Bong',
+                    ...word.slice(bIndex - 1)
+                ];
+                word = newWord;
+            }
+        }if(i % 17 == 0){
+            word.reverse();
+        }
+        if(word.length == 0){
+            console.log(i);
+            continue;
+        }
+        console.log(word.toString().replace(',', ''));
+    }
+}
